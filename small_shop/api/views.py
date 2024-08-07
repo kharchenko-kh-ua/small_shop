bff1d187c9ceef0dd7ef23dec63b44a855cc7cb1
Dmitriy Kharchenko <dmitriy.kharchenko.kh@gmail.com>
2024-08-07 23:50:06 +0000

    Backend and unit-tests

--- small_shop/api/views.py
+++ small_shop/api/views.py
@@ -1,5 +1,10 @@
+"""
+API viewsets
+"""
 from api.models import Customer
 from api.serializers import CustomerSerializer
+from api.serializers import UserSerializer
+from django.contrib.auth.models import User
 from rest_framework import permissions
 from rest_framework import viewsets
 
@@ -8,3 +13,14 @@ class CustomerViewSet(viewsets.ModelViewSet):
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all()
     permission_classes = (permissions.IsAuthenticated,)
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["request"] = self.request
+        return context
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (permissions.IsAdminUser,)
