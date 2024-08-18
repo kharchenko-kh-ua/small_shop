69ff927b56acb1ab64b684572ee365c889f0f3ee
Dmitriy Kharchenko <dmitriy.kharchenko.kh@gmail.com>
2024-08-18 23:56:39 +0000

    Production clean up

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,13 @@ then
     echo "PostgreSQL started"
 fi
 
+cat .env
+
 export $(cat .env | xargs)
 
 python manage.py migrate --no-input
 
 gunicorn small_shop.wsgi:application --bind 0.0.0.0:8000 &
-
-unlink /etc/nginx/sites-enabled/default
 
 nginx -g 'daemon off;'
 0
